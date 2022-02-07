2f4988711ee71a2eeceadd2dfc162c32b581a874
KK <karanjakelvin77@gmail.com>
2022-02-07 03:35:56 +0300

    It will import the components modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { QuotedisplayComponent } from './quotedisplay/quotedisplay.component';
+import { QuotedetailsComponent } from './quotedetails/quotedetails.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    QuotedisplayComponent,
+    QuotedetailsComponent
   ],
   imports: [
     BrowserModule,
