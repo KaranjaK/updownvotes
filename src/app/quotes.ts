c15679c5c4c4f1092ad6d13c8c1eb7980ccb8b28
KK <karanjakelvin77@gmail.com>
2022-02-08 08:34:05 +0300

    It will add the upvotes, downvotes, createdon and favoritequotes properties

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,4 +1,6 @@
 export class Quotes {
     static author: any;
-    constructor(public quote:string, public author:string, public submitter:string){}
+    constructor(public quote: string, public author: string, 
+        public submitter: string, public upvotes: number,public downvotes: number,
+        public createdOn: Date, public favoriteQuite: boolean ) { }
 }
