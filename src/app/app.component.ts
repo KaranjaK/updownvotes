47e4498eead39ca7aa8bb8b877e661b31da61460
KK <karanjakelvin77@gmail.com>
2022-02-08 08:55:00 +0300

    It will add the proeperties that have been added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,11 @@ export class AppComponent {
   title = 'updownvotes';
 
   quotes: Quotes[] = [
-    new Quotes("Yes we can", "Barack Obama", "Kelvin Karanja"),
-    new Quotes("Arise and Shine", "Prophet Isaiah", "James Mike"),
-    new Quotes("I can do all things", "Apostle Paul", "Kelvin Karanja"),
-    new Quotes("Character is key", "Anonymous", "Loren Mark"),
-    new Quotes("It is what it is", "The Ganga", "Liam")
+    new Quotes("Yes we can", "Barack Obama", "Kelvin Karanja", 0,0, new Date(),false),
+    new Quotes("Arise and Shine", "Prophet Isaiah", "James Mike", 0,0, new Date(),false),
+    new Quotes("I can do all things", "Apostle Paul", "Kelvin Karanja", 0,0, new Date(),false),
+    new Quotes("Character is key", "Anonymous", "Loren Mark", 0,0, new Date(),false),
+    new Quotes("It is what it is", "The Ganga", "Liam", 0,0, new Date(),false)
   ]
 
   addNewQuote(quote: any) {
