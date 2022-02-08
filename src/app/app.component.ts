32f2382d01aad8bc545c7c43a507b7dbfacd8ad7
KK <karanjakelvin77@gmail.com>
2022-02-08 06:23:12 +0300

    It will add more quote entries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,15 @@ import { Quotes } from './quotes'
 export class AppComponent {
   title = 'updownvotes';
 
-  quotes: Quotes[]=[
-    new Quotes ("Yes we can", "Barack Obama", "Kelvin Karanja")
+  quotes: Quotes[] = [
+    new Quotes("Yes we can", "Barack Obama", "Kelvin Karanja"),
+    new Quotes("Arise and Shine", "Prophet Isaiah", "James Mike"),
+    new Quotes("I can do all things", "Apostle Paul", "Kelvin Karanja"),
+    new Quotes("Character is key", "Anonymous", "Loren Mark"),
+    new Quotes("It is what it is", "The Ganga", "Liam")
   ]
 
-  addNewQuote (quote: any){
+  addNewQuote(quote: any) {
     this.quotes.push(quote)
   }
 }
