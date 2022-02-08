e135c768c0ca9f1537a19636bf1dcb2c4babccfc
KK <karanjakelvin77@gmail.com>
2022-02-08 04:38:04 +0300

    It will add the submission class

---
+++ src/app/submission.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Quotes } from './quotes';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubmissionService {
+
+  _url = ''
+  constructor(private _http: HttpClient) { }
+
+  submit(quote: Quotes){
+    this._http.post<any>(this._url, quote) 
+  }
+}
