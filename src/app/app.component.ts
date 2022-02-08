3279851cbd9d21c0d09bba7dbc98f1f8bf4ab9c6
KK <karanjakelvin77@gmail.com>
2022-02-08 21:14:49 +0300

    It will import and injext the submit service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Quotes } from './quotes'
+import { SubmissionService } from './submission.service';
 
 @Component({
   selector: 'app-root',
@@ -10,14 +11,16 @@ export class AppComponent {
   title = 'updownvotes';
 
   quotes: Quotes[] = [
-    new Quotes("Yes we can", "Barack Obama", "Kelvin Karanja", 0,0, new Date(),false),
-    new Quotes("Arise and Shine", "Prophet Isaiah", "James Mike", 0,0, new Date(),false),
-    new Quotes("I can do all things", "Apostle Paul", "Kelvin Karanja", 0,0, new Date(),false),
-    new Quotes("Character is key", "Anonymous", "Loren Mark", 0,0, new Date(),false),
-    new Quotes("It is what it is", "The Ganga", "Liam", 0,0, new Date(),false)
+    new Quotes(1,"Yes we can", "Barack Obama", "Kelvin Karanja", 0,0, new Date(),false),
+    new Quotes(2,"Arise and Shine", "Prophet Isaiah", "James Mike", 0,0, new Date(),false),
+    new Quotes(3,"I can do all things", "Apostle Paul", "Kelvin Karanja", 0,0, new Date(),false),
+    new Quotes(4,"Character is key", "Anonymous", "Loren Mark", 0,0, new Date(),false),
+    new Quotes(5,"It is what it is", "The Ganga", "Liam", 0,0, new Date(),false)
   ]
 
   addNewQuote(quote: any) {
     this.quotes.push(quote)
   }
+
+  constructor(private _submissionservice: SubmissionService){}
 }
