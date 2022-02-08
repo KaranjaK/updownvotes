565e983527088b16e4449ed636feab4eecee2b25
KK <karanjakelvin77@gmail.com>
2022-02-08 08:36:32 +0300

    It will add the showQuoteDetails property

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,9 @@
 export class Quotes {
-    static author: any;
-    constructor(public quote: string, public author: string, 
-        public submitter: string, public upvotes: number,public downvotes: number,
-        public createdOn: Date, public favoriteQuite: boolean ) { }
+
+    showQuoteDetails: boolean;
+    constructor(public quote: string, public author: string,
+        public submitter: string, public upvotes: number, public downvotes: number,
+        public createdOn: Date, public favoriteQuite: boolean) {
+        this.showQuoteDetails = false;
+    }
 }
