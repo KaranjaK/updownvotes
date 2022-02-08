bb148fef54b45bc3055f7a6a8827792990abef47
KK <karanjakelvin77@gmail.com>
2022-02-08 22:04:48 +0300

    It will remove the showdetail method

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,9 +1,7 @@
 export class Quotes {
-
-    showQuoteDetails: boolean;
+    
     constructor(public id: number, public quote: string, public author: string,
         public submitter: string, public upvotes: number, public downvotes: number,
-        public createdOn: Date, public favoriteQuite: boolean) {
-        this.showQuoteDetails = false;
+        public createdOn: Date, public favoriteQuote: boolean) {
     }
 }
