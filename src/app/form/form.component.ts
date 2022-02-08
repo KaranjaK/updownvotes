62eb3ab5e80f60fa0d4429b1e5d53b125816aecb
KK <karanjakelvin77@gmail.com>
2022-02-08 08:57:03 +0300

    It will define the extra properties added

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -9,6 +9,7 @@ import { Quotes } from '../quotes';
 })
 export class FormComponent implements OnInit {
 
+
   enterQuote = new Quotes('', '', '', 0,0, new Date(), false);
   @Output() addQuote = new EventEmitter<Quotes>();
 
