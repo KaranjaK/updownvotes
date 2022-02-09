99bdc7b23145f4ca4360d09d145b36334a75b702
KK <karanjakelvin77@gmail.com>
2022-02-09 06:07:03 +0300

    It will solve the import error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,17 +10,5 @@ import { SubmissionService } from './submission.service';
 export class AppComponent {
   title = 'updownvotes';
 
-  quotes: Quotes[] = [
-    new Quotes(1,"Yes we can", "Barack Obama", "Kelvin Karanja", 0,0, new Date(),false),
-    new Quotes(2,"Arise and Shine", "Prophet Isaiah", "James Mike", 0,0, new Date(),false),
-    new Quotes(3,"I can do all things", "Apostle Paul", "Kelvin Karanja", 0,0, new Date(),false),
-    new Quotes(4,"Character is key", "Anonymous", "Loren Mark", 0,0, new Date(),false),
-    new Quotes(5,"It is what it is", "The Ganga", "Liam", 0,0, new Date(),false)
-  ]
-
-  addNewQuote(quote: any) {
-    this.quotes.push(quote)
-  }
-
   constructor(private _submissionservice: SubmissionService){}
 }
