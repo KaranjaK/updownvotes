6e855ab4cd70637ae0b5594a224b70b9cb13e81a
KK <karanjakelvin77@gmail.com>
2022-02-09 03:58:13 +0300

    It will update on the server service

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.get('/', function (req, res) {
 })
 
 app.post('/quote', function(req, res){
-    console.log(req.body)
+    alert(req.body)
     res.status(200).send({'message': 'Data received'})
 })
 
