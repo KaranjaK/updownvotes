235c9665c681ffe4b8ffa6233dbb35c09b93422e
KK <karanjakelvin77@gmail.com>
2022-02-09 03:59:04 +0300

    It will adjust the submit function

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -16,7 +16,7 @@ export class FormComponent implements OnInit {
   enterQuote = new Quotes(0,'', '', '', 0,0, new Date(), false);
   @Output() addQuote = new EventEmitter<Quotes>();
   submitQuote() {
-    this.submitted = true
+    this.submitted = true;
     this._submissionservice.submit(this.enterQuote)
     .subscribe(
       data => console.log('Success', data),
