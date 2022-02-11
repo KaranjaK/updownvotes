c5041f85eaf039bca32d73157c8b6acd319636b3
KK <karanjakelvin77@gmail.com>
2022-02-11 03:47:16 +0300

    It will define the date element

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -5,12 +5,10 @@ export class Quotes {
 
     public upvotes: number;
     public downvotes: number;
-    public createdOn: Date;
     
     constructor(public quote: string, public author: string,
-        public submitter: string) {
+        public submitter: string, public createdOn: Date) {
             this.upvotes = 0
             this.downvotes = 0
-            this.createdOn = new Date()
     }
 }
